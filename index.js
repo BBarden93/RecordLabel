71b6aabca97bedb90b1556bef0d6928c333e746d
Brittany Barden <bbarden93@yahoo.com>
2018-03-16 10:10:33 -0700

    set up model

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const
   logger = require('morgan'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
-  port = process.env.port || 3000
+  port = process.env.port || 3000,
+  Album = require('./models/Album.js')
 
 mongoose.connect('mongodb://localhost/record-label', (err) => {
   console.log(err || "Connected to MongoDB.")
--- models/Album.js
+++ models/Album.js
@@ -1,1 +1,11 @@
 // Album model here...
+const 
+    mongoose = require('mongoose'),
+    albumSchema = new mongoose.Schema({
+        title: String,
+        releaseDate: Date,
+        albumArt: String
+    }, {timestamps: true})
+
+    const Album = mongoose.model('Album', albumSchema)
+    module.exports = Album
